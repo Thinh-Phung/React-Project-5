cc6c260b7e5434e5449252d5c50019e5075bf121
Thinh-Phung <phunghuuthinh01@gmail.com>
2023-01-30 16:50:48 +0700

    add exercises videos

---
+++ src/components/SimilarExercises.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
+  return <Box sx={{ mt: { lg: "100px", xs: "0" } }}></Box>;
+};
+
+export default SimilarExercises;
